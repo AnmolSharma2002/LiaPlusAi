165e6237a0a04eafdb4f6bbc8782946c2ef669a8
Anmol Sharma <anmol0602sharma@gmail.com>
2025-04-26 07:30:07 +0530

    Delete liaapi directory

--- liaapi/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-
-router.post("/signup", authController.signup);
-router.post("/login", authController.login);
-router.get("/verify-email", authController.verifyEmail);
-module.exports = router;
--- liaapi/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authRoutes = require("../routes/authRoutes");
-const blogRoutes = require("../routes/blogRoutes");
-router.use("/auth", authRoutes);
-router.use("/blogs", blogRoutes);
-module.exports = router;
